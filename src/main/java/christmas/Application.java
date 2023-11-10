ff21ea058e13f6bb9d4b9b0812e705e11fe179f5
IronD414 <kcy1760@gmail.com>
2023-11-10 16:37:15 +0900

    feat: validate order menu format

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,14 +2,31 @@ package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-
+        Map<String, Integer> menuAppetizer = Map.of(
+                "양송이수프", 6000,
+                "타파스", 5500,
+                "시저샐러드", 8000
+        );
+        Map<String, Integer> menuMain = Map.of(
+                "티본스테이크", 55000,
+                "바비큐립", 54000,
+                "해산물파스타", 35000,
+                "크리스마스파스타", 25000
+        );
+        Map<String, Integer> menuDessert = Map.of(
+                "초코케이크", 15000,
+                "아이스크림", 5000
+        );
+        Map<String, Integer> menuDrink = Map.of(
+                "제로콜라", 3000,
+                "레드와인", 60000,
+                "샴페인", 25000
+        );
+        List<Map<String, Integer>> menus = List.of(menuAppetizer, menuMain, menuDessert, menuDrink);
 
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
@@ -20,10 +37,10 @@ public class Application {
         while (true){
             userInput = Console.readLine();
             try{
-                visitingDay = Integer.parseInt(userInput);
-                if (visitingDay < 1 || visitingDay > 31) throw new NumberFormatException();
+                visitingDay = Integer.parseInt(userInput);  //  NumberFormatException -> IllegalArgumentException 변환을 위해 Integer.parseInt()를 메서드로 뺄 필요 있음
+                if (visitingDay < 1 || visitingDay > 31) throw new IllegalArgumentException();
                 break;
-            }catch (NumberFormatException e){
+            }catch (IllegalArgumentException e){
                 System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             }
         }
@@ -32,14 +49,25 @@ public class Application {
 
         Map<String, Integer> order = new HashMap<>();
         while (true){
-            userInput = Console.readLine();
+            try{
+                userInput = Console.readLine();
 
-            String[] splitUserInput = userInput.split(",");
-            for (String eachUserInput : splitUserInput){
-                String[] splitEachUserInput = eachUserInput.split("-");
-                order.put(splitEachUserInput[0], Integer.parseInt(splitEachUserInput[1]));
+                String[] splitUserInput = userInput.split(",");
+                for (String eachUserInput : splitUserInput){
+                    if (!eachUserInput.contains("-")) throw new IllegalArgumentException();
+
+                    String[] splitEachUserInput = eachUserInput.split("-");
+                    String userInputMenuName = splitEachUserInput[0];
+                    int userInputMenuQuantity = Integer.parseInt(splitEachUserInput[1]);
+
+                    if (userInputMenuQuantity < 1) throw new IllegalArgumentException();
+
+                    order.put(userInputMenuName, userInputMenuQuantity);
+                }
+                break;
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
             }
-            break;
         }
     }
 }

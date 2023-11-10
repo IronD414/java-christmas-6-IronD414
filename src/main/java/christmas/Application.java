00fa1b1eaaad6be2d9d50ea7754802fb26397266
IronD414 <kcy1760@gmail.com>
2023-11-10 15:50:56 +0900

    feat: print request order menu

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -22,5 +22,7 @@ public class Application {
                 System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             }
         }
+
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
     }
 }

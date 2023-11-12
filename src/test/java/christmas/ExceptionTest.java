f95cde9e05760f30daf0924c4ad4fceecbc9573e
IronD414 <kcy1760@gmail.com>
2023-11-12 17:20:04 +0900

    test: exception drink order only

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -1,12 +1,10 @@
 package christmas;
 
-import christmas.domain.model.*;
+import christmas.constants.ErrorMessages;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,8 +30,28 @@ public class ExceptionTest {
 
                 Application.main(new String[]{});
 
-                assertThat(outputStreamCaptor.toString()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                assertThat(outputStreamCaptor.toString()).contains(ErrorMessages.INVALID_DATE.getMessage());
             }
+        }catch (final NoSuchElementException ignore) {
+        }finally {
+            System.setOut(originalOut);
+            System.setIn(originalIn);
+        }
+    }
+    @DisplayName("음료만_주문_예외_테스트")
+    @Test
+    void checkDrinkOrderOnly(){
+        try {
+            System.setOut(new PrintStream(outputStreamCaptor));
+
+            String input = "1\n" + "레드와인-3,샴페인-2\n";
+
+            inputStream = new ByteArrayInputStream(input.getBytes());
+            System.setIn(inputStream);
+
+            Application.main(new String[]{});
+
+            assertThat(outputStreamCaptor.toString()).contains(ErrorMessages.INVALID_ORDER.getMessage());
         }catch (final NoSuchElementException ignore) {
         }finally {
             System.setOut(originalOut);
